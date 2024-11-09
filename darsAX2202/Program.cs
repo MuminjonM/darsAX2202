e4c36d83e79ae18608c485b0cabb7fb558c42f5f
MuminjonM <m.o.murodullayev@gmail.com>
2024-11-09 10:55:49 +0500

    2ta sonning yigindisi

---
+++ darsAX2202/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace darsAX2202
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 39,b=50;
+
+            Console.WriteLine("a+b=" + (int)(a + b));
+            Console.ReadKey();
+
+
+        }
+    }
+}
